3b4bbd4b866dfb1c7d2a9fa65ce7ed1973b76d0a
German Gamboa <germangamboa95@gmail.com>
2018-04-10 12:31:33 -0400

    firebase added

---
+++ main.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDw6Eov1v2G0QfdsVqj3D98iefXgX-D0UA",
+  authDomain: "feedback-5b053.firebaseapp.com",
+  databaseURL: "https://feedback-5b053.firebaseio.com",
+  projectId: "feedback-5b053",
+  storageBucket: "feedback-5b053.appspot.com",
+  messagingSenderId: "90869171582"
+};
+firebase.initializeApp(config);
