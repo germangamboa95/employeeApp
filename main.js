186655993820a0d3cf8314e81bf980d4553804ef
German Gamboa <germangamboa95@gmail.com>
2018-04-10 12:42:44 -0400

    all database is done

--- main.js
+++ main.js
@@ -8,3 +8,35 @@ var config = {
   messagingSenderId: "90869171582"
 };
 firebase.initializeApp(config);
+
+const database = firebase.database();
+
+const employee_model = {
+  name: "German Gamboa",
+  role: "Manager",
+  start_date: 2000,
+  monthly_rate: 100
+}
+
+database.ref('employee').push().set(employee_model);
+
+
+database.ref('employee').on('child_added', function(snapshot) {
+  console.log(snapshot.val());
+});
+
+
+addEmployee('Shahar','designer', '1234', '9000');
+
+
+function addEmployee(name, role, start_date, monthly_rate ) {
+  const employee_model = {
+    name: name,
+    role: role,
+    start_date: start_date,
+    monthly_rate: monthly_rate
+  }
+
+  database.ref('employee').push().set(employee_model);
+
+}
