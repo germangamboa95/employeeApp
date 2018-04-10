60059e112f323fd7a90c2c2a41c3c6752a58fba5
German Gamboa <germangamboa95@gmail.com>
2018-04-10 13:02:17 -0400

    added adf ds

--- main.js
+++ main.js
@@ -11,30 +11,24 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-const employee_model = {
-  name: "German Gamboa",
-  role: "Manager",
-  start_date: 2000,
-  monthly_rate: 100
-}
-
-database.ref('employee').push().set(employee_model);
 
 
-database.ref('employee').on('child_added', function(snapshot) {
+// New employee added event listener
+database.ref('employee').on('value', function(snapshot) {
   console.log(snapshot.val());
 });
 
 
 addEmployee('Shahar','designer', '1234', '9000');
 
-
+// add employee to database
 function addEmployee(name, role, start_date, monthly_rate ) {
   const employee_model = {
     name: name,
     role: role,
     start_date: start_date,
-    monthly_rate: monthly_rate
+    monthly_rate: monthly_rate,
+    added_date: firebase.database.ServerValue.TIMESTAMP
   }
 
   database.ref('employee').push().set(employee_model);
@@ -42,4 +36,6 @@ function addEmployee(name, role, start_date, monthly_rate ) {
 }
 
 
-function
+function add_row() {
+
+}
